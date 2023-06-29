6dd6be286d12a05cd526550f54daa2a7f28c15ac
UTHRATHITHAN <m.uthrathithan@gmail.com>
2023-06-29 20:45:51 +0530

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logoSvg from "../assets/logo.svg";
+
+const Logo = () => {
+  return (
+    <Link
+      to="/"
+      className="
+     absolute top-[1.5rem] left-[1.5rem] [text-decoration:none]
+text-lg text-cyan flex items-center
+     "
+    >
+      <img src={logoSvg} alt="CryptoBucks"  />
+      <span>Cryptocurrency Tracker</span>
+
+      
+    </Link>
+  );
+};
+
+export default Logo;
